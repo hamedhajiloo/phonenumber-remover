8fc8a245f7d8f372a8b20f681603f9aaa736794e
hamedhajiloo <hamedhajiloo@hotmail.com>
2021-12-14 20:09:05 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+while (true)
+{
+    Console.WriteLine("Enter the text...");
+    var input = Console.ReadLine();
+    if (input == "exit") break;
+    if (input == "quit") break;
+    if (input is null) continue;
+
+    var rejex = new Regex(@"(\+98|0)?9\d{9}");
+    var result = rejex.Replace(input, "");
+    Console.WriteLine($"result: {result}");
+    Console.WriteLine("_____________________");
+    Console.WriteLine();
+}
